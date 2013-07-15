92315bbca061d415f283cb6b0d1eba7a006b8f35
Aaron Davis <aaronzdavis@gmail.com>
2013-07-15 00:57:38 -0700

    update Specs, remove UPS credentials

--- spec/ups_shipping_spec.rb
+++ spec/ups_shipping_spec.rb
@@ -1,11 +1,19 @@
 require "rspec"
 require "ups_shipping"
-require "address"
-require "organization"
 
 describe Shipping::UPS do
   before(:all) do
-    @ups = Shipping::UPS.new("deanchen", "Transcriptic#1", "EC96D31A8D672E28", :test => true)
+    SPEC_ROOT = File.dirname(__FILE__).freeze
+    ups_credentials_path = "#{SPEC_ROOT}/config/ups_credentials.yml"
+
+    if !File.exists?(ups_credentials_path)
+      raise "You need to add your own UPS test credentials in spec/config/ups_credentials.yml. See spec/config/ups_credentials.example.yml for an example."
+    end
+
+    ups_credentials = YAML.load(File.read(ups_credentials_path))
+
+    @ups = Shipping::UPS.new(ups_credentials["account"], ups_credentials["password"], ups_credentials["key"], :test => false)
+
     @address = Shipping::Address.new(
         :address_lines => ["1402 Faber St."],
         :city => "Durham",
@@ -14,6 +22,7 @@ describe Shipping::UPS do
         :country => "US"
     )
   end
+
   it "address object" do
     @address.to_xml.gsub(/^\s+/, "").gsub(/\s+$/, $/).should == '
       <?xml version="1.0"?>
@@ -26,6 +35,7 @@ describe Shipping::UPS do
       </Address>
       '.gsub(/^\s+/, "").gsub(/\s+$/, $/)
   end
+
   it "organization object" do
     @organization = Shipping::Organization.new(
         :name => "Transcriptic",
@@ -34,8 +44,15 @@ describe Shipping::UPS do
     )
     puts @organization.to_xml("Shipper")
   end
+
+  it "validates address" do
+    @validate_address = @ups.validate_address(@address)
+    @validate_address["AddressValidationResponse"]["Response"]["ResponseStatusCode"].should == "1"
+    puts @validate_address
+  end
+
   it "#track_shipment" do
-    tracking_result = @ups.track_shipment("1ZXX31290231000092")
+    tracking_result = @ups.track_shipment("1ZYA52510353561107")
     tracking_result.should have_key("TrackResponse")
     tracking_result["TrackResponse"]["Response"]["ResponseStatusCode"].should == "1"
   end
